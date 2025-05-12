6bee03b0a848b2b11c19c30a6184af27c0bc196e
Ram Sharan Rimal <rsr0012@uah.edu>
2025-05-12 11:36:54 -0500

    fix duplication

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-export { EmitInterface } from './pages/EmitInterface';
-export type { EmitInterfaceProps } from './pages/EmitInterface';
-
-// Export types for configuration
-export type { Config } from './services/config'; 
