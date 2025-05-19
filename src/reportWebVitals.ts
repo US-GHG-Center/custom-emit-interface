95b35a7265fc77d361daac37848a18f6c7970a9e
Ram Sharan Rimal <rsr0012@uah.edu>
2025-05-19 14:15:41 -0500

    fix report web

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,14 +1,32 @@
+// src/reportWebVitals.ts
 import { ReportHandler } from 'web-vitals';
 
-const reportWebVitals = (onPerfEntry?: ReportHandler) => {
+const reportWebVitals = (onPerfEntry?: ReportHandler): void => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+    import('web-vitals')
+      .then(({ onCLS, onINP, onFCP, onLCP, onTTFB }) => {
+        if (onCLS) {
+          onCLS(onPerfEntry);
+        }
+        if (onINP) {
+          onINP(onPerfEntry);
+        }
+        if (onFCP) {
+          onFCP(onPerfEntry);
+        }
+        if (onLCP) {
+          onLCP(onPerfEntry);
+        }
+        if (onTTFB) {
+          onTTFB(onPerfEntry);
+        }
+      })
+      .catch((err) => {
+        console.error(
+          'Failed to load web-vitals module or an error occurred during import:',
+          err
+        );
+      });
   }
 };
 
